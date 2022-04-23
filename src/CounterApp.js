98b55e7d235712a617224c2c858e9bb5c24ea441
emanuelhardwell <emanuelnoe16@gmail.com>
2022-04-23 16:29:04 -0500

    Se agrego la aplicacion de conterApp

---
+++ src/CounterApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    //forma 2
+    // setCounter((c) => c + 1);
+  };
+
+  const handleSubstract = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+
+  return (
+    <div>
+      <h1> CounterApp </h1>
+      <h2>counter: </h2>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}> +1 </button>
+      <button onClick={handleSubstract}> -1 </button>
+      <button onClick={handleReset}> reset </button>
+    </div>
+  );
+};
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppDefaultProps from "./AppDefaultProps";
+import CounterApp from "./CounterApp";
+// import AppDefaultProps from "./AppDefaultProps";
 // import AppPropTypes from "./AppPropTypes";
 import "./index.css";
 // import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppDefaultProps saludo="saludo principal" />);
+root.render(<CounterApp value={10} />);
